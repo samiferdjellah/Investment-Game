22d69086c1b5de695e7181a641651e768ad334a1
sami <sami.ferdjellah@vt.atlascopco.com>
2024-12-03 16:07:58 +0100

    update Sami his work theme

--- Investment_Game_script.py
+++ Investment_Game_script.py
@@ -1,2 +1,4 @@
 # Investment-Game.py
-# frank is working on Create different kinds of orders (buy, sell, limits, stop orders, etc.)
+# frank is working on, "Create different kinds of orders (buy, sell, limits, stop orders, etc.)"
+# tom is working in,  
+# sami is working on, "A GUI (web)."
