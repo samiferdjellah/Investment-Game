6fb0cec842f0057b32beb31d22fa681727f4d5ae
sami <sami.ferdjellah@vt.atlascopco.com>
2024-12-03 16:49:47 +0100

    GUI buy, sell, limits, stop orders_V1

--- GUI investment game.py
+++ GUI investment game.py
@@ -3,16 +3,16 @@ class order_type:
     def __init__(self):
         order_input = input("what do you want to do, buy or sell your stocks ?")
         if order_input = "sell":
-            print(selling stocks)
+            print("selling stocks")
         else:
-            print(order stocks)
+            print("order stocks")
     
-    def limit risks
+    def limit_risks(self):
         order_input = input("do you want to create a safety price limit of your stocks or place a stop orders")
         if order_input = "safety price limit":
-            print(safety price limit)
+            print("safety price limit")
         else:
-            print(stop orders)
+            print("stop orders")
 
 
 order_type()
