115836d91a8a831a730024b729baeb6e0237eccb
sami <sami.ferdjellah@vt.atlascopco.com>
2024-12-03 16:37:46 +0100

    GUI investment game script

---
+++ GUI investment game.py
@@ -0,0 +1,8 @@
+# GUI investment game.py
+class order_type:
+    def __init__(self):
+        order_input = input("what do you want to do, buy or sell your stocks ?")
+        if order_input = "sell":
+            print(selling stocks)
+        else:
+            print(order stocks)
