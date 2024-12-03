98ce7a07c2ae082dfddaee52f2c9f9f8f3f5a7a2
Tom De Vilder <tom.de.vilder@atlascopco.com>
2024-12-03 16:35:37 +0100

    update Tom

--- Investment_Game_script.py
+++ Investment_Game_script.py
@@ -1,1 +1,9 @@
 # Investment-Game.py
+from person import Person
+
+if __name__ == "__main__":
+    person = Person()
+    firstName = person.getFirstName()
+    lastName = person.getLastName()
+    email = person.getEmail()
+    print(person)
